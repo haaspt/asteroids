caad237f73270e8972fd5fd37e2ca47ec8ea8950
Patrick Tyler Haas <patrick.tyler.haas@gmail.com>
2019-04-25 20:57:03 -0700

    Tweak: switching out sprites

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -8,6 +8,9 @@ func new_game():
 	score = 0
 	$HUD.update_score(score)
 	$Player.start($PlayerStartPosition.position)
+	
+func game_over():
+	$HUD.display_game_over()
 
 func _on_Player_did_shoot(pos, rot):
 	var bullet = Bullet.instance()
